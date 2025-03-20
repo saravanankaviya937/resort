adfa1375cdaaff530f806782af23d84ae95ee3b4
saravanankaviya937 <saravanankaviya937@gmail.com>
2025-03-20 15:18:15 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -96,27 +96,8 @@ import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={glyn2} className="d-block w-100" alt="Apartment 1" height="500px" />
-          </div>
-          <div className="carousel-item">
-            <img src={glyn6} className="d-block w-100" alt="Apartment 3" height="500px" />
-          </div>
-          <div className="carousel-item">
-            <img src={glyn18} className="d-block w-100" alt="Apartment 2" height="500px" />
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+      
+      
       <div className="text-center mt-4">
        <h6 style={{ color: "orange", fontSize: "x-large", position: "relative", display: "inline-block", paddingBottom: "5px" }}>
          Welcome to Glyngarth
